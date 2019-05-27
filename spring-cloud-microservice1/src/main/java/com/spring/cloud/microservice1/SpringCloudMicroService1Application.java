b2df35b877c7358f787af31bbad294436a343405
limengcanyu <limengcanyu@163.com>
2019-05-27 21:01:12 +0800

    add swagger

--- spring-cloud-microservice1/src/main/java/com/spring/cloud/microservice1/SpringCloudMicroService1Application.java
+++ spring-cloud-microservice1/src/main/java/com/spring/cloud/microservice1/SpringCloudMicroService1Application.java
@@ -1,11 +1,14 @@
 package com.spring.cloud.microservice1;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Spring Cloud MicroService1 Application
@@ -13,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @author jxf
  * @date 2019/5/26
  */
+@Api("hello app")
 @RestController
+@EnableSwagger2
 @SpringBootApplication
 public class SpringCloudMicroService1Application {
-    @RequestMapping("/")
+    @ApiOperation(value = "获取用户列表", notes = "获取所有用户信息")
+    @GetMapping("/")
     public String home() {
         return "Hello world";
     }
