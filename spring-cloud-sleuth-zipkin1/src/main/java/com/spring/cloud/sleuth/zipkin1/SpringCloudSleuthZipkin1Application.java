78d87629bf1f94a2ec9deb836694d0c4a5ee517c
jiangxf <limengcanyu@163.com>
2019-10-30 15:16:16 +0800

    update spring-cloud-sleuth

--- spring-cloud-sleuth-zipkin1/src/main/java/com/spring/cloud/sleuth/zipkin1/SpringCloudSleuthZipkin1Application.java
+++ spring-cloud-sleuth-zipkin1/src/main/java/com/spring/cloud/sleuth/zipkin1/SpringCloudSleuthZipkin1Application.java
@@ -1,10 +1,30 @@
 package com.spring.cloud.sleuth.zipkin1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudSleuthZipkin1Application {
+    private static Logger logger = LoggerFactory.getLogger(SpringCloudSleuthZipkin1Application.class);
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello spring-cloud-sleuth-zipkin1";
+    }
+
+    @GetMapping("/echo/{string}")
+    public String echo(@PathVariable String string) {
+        logger.info("spring-cloud-sleuth-zipkin1 Hello " + string);
+        return "spring-cloud-sleuth-zipkin1 Hello " + string;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudSleuthZipkin1Application.class, args);
--- spring-cloud-sleuth-zipkin2/src/main/java/com/spring/cloud/sleuth/zipkin2/SpringCloudSleuthZipkin2Application.java
+++ spring-cloud-sleuth-zipkin2/src/main/java/com/spring/cloud/sleuth/zipkin2/SpringCloudSleuthZipkin2Application.java
@@ -1,10 +1,36 @@
 package com.spring.cloud.sleuth.zipkin2;
 
+import com.spring.cloud.sleuth.zipkin2.feignclient.EchoClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableFeignClients
 @SpringBootApplication
 public class SpringCloudSleuthZipkin2Application {
+    private static Logger logger = LoggerFactory.getLogger(SpringCloudSleuthZipkin2Application.class);
+
+    @Autowired
+    private EchoClient echoClient;
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello spring-cloud-sleuth-zipkin2";
+    }
+
+    @GetMapping("/echo/{str}")
+    public String echo(@PathVariable String str) {
+        logger.info("echo " + str);
+        return echoClient.echo("alita");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudSleuthZipkin2Application.class, args);
