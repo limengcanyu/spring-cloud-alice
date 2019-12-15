d27d20646783aaf86f0e442bcadee21669dd69d5
limengcanyu <limengcanyu@163.com>
2019-12-15 14:53:15 +0800

    update spring-cloud-zuul

---
+++ spring-cloud-zuul/src/main/java/com/spring/cloud/zuul/config/MyWebMvcConfig.java
@@ -0,0 +1,65 @@
+package com.spring.cloud.zuul.config;
+
+import com.spring.cloud.zuul.interceptor.WebSecurityInterceptor;
+import com.spring.cloud.zuul.interceptor.WeixinSecurityInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 配置拦截器类
+ */
+@Configuration
+public class MyWebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private WebSecurityInterceptor webSecurityInterceptor;
+
+    @Autowired
+    private WeixinSecurityInterceptor weixinSecurityInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+    /**
+     * 拦截器按添加顺序依次执行
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // swagger-ui、注册、登录不拦截
+        registry.addInterceptor(webSecurityInterceptor).addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/swagger-resources/**",
+                        "/webjars/**",
+                        "/v2/**",
+                        "/swagger-ui.html/**",
+                        "/api/download/**",
+                        "/api/export/**",
+                        "/api/upload/**"
+//                        UriConstant.URI_REGISTER,
+//                        UriConstant.URI_LOGIN
+                );
+        registry.addInterceptor(weixinSecurityInterceptor);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //swagger增加url映射
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
---
+++ spring-cloud-zuul/src/main/java/com/spring/cloud/zuul/interceptor/WebSecurityInterceptor.java
@@ -0,0 +1,29 @@
+package com.spring.cloud.zuul.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <p>Description: Web SecurityInterceptor</p>
+ *
+ * @author rock.jiang
+ * Date 2019/11/28 17:18
+ */
+@Component
+public class WebSecurityInterceptor extends HandlerInterceptorAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(WebSecurityInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.debug("WebSecurityInterceptor 验证 开始 当前线程ID: {} 线程名称: {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+
+        logger.debug("WebSecurityInterceptor 验证 结束");
+        return true;
+    }
+
+}
