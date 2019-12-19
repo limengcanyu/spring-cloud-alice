cbf6fba92257d50b0e56e929abad77908b6f6013
limengcanyu <limengcanyu@163.com>
2019-12-19 21:26:09 +0800

    update spring-cloud-gateway

--- spring-cloud-gateway/README.md
+++ spring-cloud-gateway/README.md
@@ -1,10 +1,84 @@
-# Spring Cloud Gateway features
+# spring-cloud-gateway
 
-- Built on Spring Framework 5, Project Reactor and Spring Boot 2.0
-- Able to match routes on any request attribute.
-- Predicates and filters are specific to routes.
-- Hystrix Circuit Breaker integration.
-- Spring Cloud DiscoveryClient integration
-- Easy to write Predicates and Filters
-- Request Rate Limiting
-- Path Rewriting
+# Eureka Server
+
+http://localhost:8761
+
+# route microservice1
+
+http://localhost:8770/microservice1/echo/str
+
+http://localhost:8770/spring-cloud-microservice1/echo/str
+
+服务启动3个实例，可以负载均衡
+
+# route microservice2
+
+http://localhost:8770/microservice2/
+
+http://localhost:8770/spring-cloud-microservice2/
+
+# see actuator
+
+http://localhost:8770/actuator
+
+# see actuator routes
+
+http://localhost:8770/actuator/gateway/routes
+
+输出:
+
+[
+	{
+		"predicate": "Paths: [/spring-cloud-microservice2/**], match trailing slash: true",
+		"route_id": "ReactiveCompositeDiscoveryClient_SPRING-CLOUD-MICROSERVICE2",
+		"filters": [
+			"[[RewritePath /spring-cloud-microservice2/(?<remaining>.*) = '/${remaining}'], order = 1]"
+		],
+		"uri": "lb://SPRING-CLOUD-MICROSERVICE2",
+		"order": 0
+	},
+	{
+		"predicate": "Paths: [/spring-cloud-microservice1/**], match trailing slash: true",
+		"route_id": "ReactiveCompositeDiscoveryClient_SPRING-CLOUD-MICROSERVICE1",
+		"filters": [
+			"[[RewritePath /spring-cloud-microservice1/(?<remaining>.*) = '/${remaining}'], order = 1]"
+		],
+		"uri": "lb://SPRING-CLOUD-MICROSERVICE1",
+		"order": 0
+	},
+	{
+		"predicate": "Paths: [/spring-cloud-gateway/**], match trailing slash: true",
+		"route_id": "ReactiveCompositeDiscoveryClient_SPRING-CLOUD-GATEWAY",
+		"filters": [
+			"[[RewritePath /spring-cloud-gateway/(?<remaining>.*) = '/${remaining}'], order = 1]"
+		],
+		"uri": "lb://SPRING-CLOUD-GATEWAY",
+		"order": 0
+	},
+	{
+		"predicate": "Paths: [/microservice1/**], match trailing slash: true",
+		"route_id": "microservice1_route",
+		"filters": [
+			"[[StripPrefix parts = 1], order = 1]"
+		],
+		"uri": "lb://spring-cloud-microservice1",
+		"order": 0
+	},
+	{
+		"predicate": "Paths: [/microservice2/**], match trailing slash: true",
+		"route_id": "microservice2_route",
+		"filters": [
+			"[[StripPrefix parts = 1], order = 1]"
+		],
+		"uri": "lb://spring-cloud-microservice2",
+		"order": 0
+	}
+]
+
+由输出可知，gateway的路由不仅可以通过指定配置的Paths，例如：/microservice1/**，还可以通过服务的Eureka实例名称，例如：/spring-cloud-microservice1/**
+因此以下两种是相同效果：
+http://localhost:8770/microservice1/echo/str
+http://localhost:8770/spring-cloud-microservice1/echo/str
+
+
--- spring-cloud-gateway/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayApplication.java
@@ -2,61 +2,54 @@ package com.spring.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
-@RestController
-@EnableConfigurationProperties(UriConfiguration.class)
+//@RestController
+//@EnableConfigurationProperties(UriConfiguration.class)
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudGatewayApplication.class, args);
     }
 
-    @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
-
-        String httpUri = uriConfiguration.getHttpbin();
-
-        return builder.routes()
-                // 到 http://localhost:8770/get 的get请求被路由到 https://httpbin.org/get
-                .route(p -> p
-                        .path("/get")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri(httpUri))
-                // 到 http://localhost:8770/delay/3 的get请求被熔断 请求header中需包含：key: Host value: www.hystrix.com
-                .route(p -> p
-                        .host("*.hystrix.com")
-                        .filters(f -> f
-                                .hystrix(config -> config
-                                        .setName("mycmd")
-                                        .setFallbackUri("forward:/fallback")))
-                        .uri(httpUri))
-                .build();
-    }
-
-    @RequestMapping("/fallback")
-    public Mono<String> fallback() {
-        return Mono.just("fallback");
-    }
-}
-
-@ConfigurationProperties
-class UriConfiguration {
-    private String httpbin = "http://httpbin.org:80";
-
-    public String getHttpbin() {
-        return httpbin;
-    }
-
-    public void setHttpbin(String httpbin) {
-        this.httpbin = httpbin;
-    }
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+//
+//        String httpUri = uriConfiguration.getHttpbin();
+//
+//        return builder.routes()
+//                // 到 http://localhost:8770/get 的get请求被路由到 https://httpbin.org/get
+//                .route(p -> p
+//                        .path("/get")
+//                        .filters(f -> f.addRequestHeader("Hello", "World"))
+//                        .uri(httpUri))
+//                // 到 http://localhost:8770/delay/3 的get请求被熔断 请求header中需包含：key: Host value: www.hystrix.com
+//                .route(p -> p
+//                        .host("*.hystrix.com")
+//                        .filters(f -> f
+//                                .hystrix(config -> config
+//                                        .setName("mycmd")
+//                                        .setFallbackUri("forward:/fallback")))
+//                        .uri(httpUri))
+//                .build();
+//    }
+//
+//    @RequestMapping("/fallback")
+//    public Mono<String> fallback() {
+//        return Mono.just("fallback");
+//    }
+//}
+//
+//@ConfigurationProperties
+//class UriConfiguration {
+//    private String httpbin = "http://httpbin.org:80";
+//
+//    public String getHttpbin() {
+//        return httpbin;
+//    }
+//
+//    public void setHttpbin(String httpbin) {
+//        this.httpbin = httpbin;
+//    }
 }
 
