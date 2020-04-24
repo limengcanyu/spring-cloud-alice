522e7ed39da79f71d9da1373fa7410c461f83cc2
limengcanyu <limengcanyu@163.com>
2020-04-24 20:29:17 +0800

    update only leave spring-cloud

--- spring-cloud-admin-server/README.md
+++
@@ -1,1 +0,0 @@
-# spring boot admin server
--- spring-cloud-commons/src/main/java/com/spring/cloud/commons/dto/BaseDto.java
+++
@@ -1,9 +0,0 @@
-package com.spring.cloud.commons.dto;
-
-import lombok.Data;
-
-@Data
-public class BaseDto {
-    private Integer current = 1;
-    private Integer size = 10;
-}
