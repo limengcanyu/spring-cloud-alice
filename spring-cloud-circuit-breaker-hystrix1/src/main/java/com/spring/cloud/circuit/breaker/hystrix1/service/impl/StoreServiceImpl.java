8237006baa8fe8787dbebb1c18a2172b82e612d8
limengcanyu <limengcanyu@163.com>
2020-08-22 23:16:56 +0800

    update spring-cloud-circuit-breaker-hystrix

--- spring-cloud-circuit-breaker-hystrix1/src/main/java/com/spring/cloud/circuit/breaker/hystrix1/service/impl/StoreServiceImpl.java
+++ spring-cloud-circuit-breaker-hystrix1/src/main/java/com/spring/cloud/circuit/breaker/hystrix1/service/impl/StoreServiceImpl.java
@@ -2,6 +2,7 @@ package com.spring.cloud.circuit.breaker.hystrix1.service.impl;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.spring.cloud.circuit.breaker.hystrix1.service.StoreService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -13,12 +14,14 @@ import java.util.concurrent.TimeUnit;
  * @author rock.jiang
  * Date 2020/04/19 19:41
  */
+@Slf4j
 @Service
 public class StoreServiceImpl implements StoreService {
 
 //    @HystrixCommand(fallbackMethod = "defaultStores" )
     @Override
     public String getStores(Map<String, Object> parameters) throws Exception {
+        log.debug("====== getStores");
         //do stuff that might fail
         // 正常返回
 //        return "normal return";
