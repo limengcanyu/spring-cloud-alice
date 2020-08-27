1bf6d15bc8703c59638adac6bbf100929ac509ed
Rock Jiang <limengcanyu@163.com>
2020-08-27 11:53:07 +0800

    add spring-cloud-gateway-sentinel resilience4j

---
+++ spring-cloud-gateway-resilience4j/src/main/java/com/spring/cloud/gateway/resilience4j/SpringCloudGatewayResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.gateway.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayResilience4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayResilience4jApplication.class, args);
+    }
+
+}
