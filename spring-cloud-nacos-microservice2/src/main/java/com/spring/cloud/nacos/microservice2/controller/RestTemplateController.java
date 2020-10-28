b7554d9bd49b0f957b1d35a89abba913a505a213
Rock Jiang <limengcanyu@163.com>
2020-10-28 11:19:52 +0800

    update 20201028

--- spring-cloud-nacos-microservice2/src/main/java/com/spring/cloud/nacos/microservice2/controller/RestTemplateController.java
+++ spring-cloud-nacos-microservice2/src/main/java/com/spring/cloud/nacos/microservice2/controller/RestTemplateController.java
@@ -1,6 +1,6 @@
 package com.spring.cloud.nacos.microservice2.controller;
 
-import com.spring.cloud.nacos.microservice2.feignClient.Service3Client;
+import com.spring.cloud.nacos.microservice2.feign.Service3Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
