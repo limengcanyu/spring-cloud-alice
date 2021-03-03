131692562fa231b7fcc4a8b964ddc293d9030b4f
limengcanyu <limengcanyu@163.com>
2021-03-03 17:43:53 +0800

    update spring-cloud-nacos-microservice2

--- spring-cloud-nacos-microservice2/src/main/java/com/spring/cloud/nacos/microservice2/config/ApplicationConfig.java
+++ spring-cloud-nacos-microservice2/src/main/java/com/spring/cloud/nacos/microservice2/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.spring.cloud.nacos.microservice2.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationConfig {
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
